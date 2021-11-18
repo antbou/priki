3e45d3b9fceb228dfd208cf31faba1937255bb8e
Anthony Bouillant <anthony.bouillant@cpnv.ch>
2021-11-18 10:34:16 +0100

    Create seeder

--- database/seeders/DomainSeeder.php
+++ database/seeders/DomainSeeder.php
@@ -2,19 +2,18 @@
 
 namespace Database\Seeders;
 
+use App\Models\Domain;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class DomainSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        $this->call([
-            DomainSeeder::class,
-        ]);
+        Domain::create(['name' => 'Java', 'slug' => 'JAV']);
     }
 }
