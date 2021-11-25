d0648c3e352599abe123026bd6b91b93d7141eac
Anthony <anthony.bouillant@cpnv.ch>
2021-11-25 20:39:15 +0100

    Update HomeController not useful anymore

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,21 +2,11 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Practice;
-use Carbon\Carbon;
-use Illuminate\Http\Request;
-use App\Models\PublicationState;
 
 class HomeController extends Controller
 {
-    public function show(Request $request)
+    public function show()
     {
-        Carbon::setLocale('fr');
-
-        $days = ($request->has('days') && is_numeric($request->input('days'))) ? $request->input('days') : Practice::DAYS;
-
-        $practices = PublicationState::where('slug', 'PUB')->first()->practices()->where('updated_at', '>=', Carbon::now()->subDays($days))->get();
-
-        return view('home.home', ['practices' => $practices]);
+        return view('home.home');
     }
 }
--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -1,28 +1,7 @@
-@extends('layout')
+{{-- @extends('layout')
 @section('title', 'HomePage')
 @section('content')
-    <article class="text-3xl max-w-prose mx-auto py-8">
 
-        <div class="flex justify-around pb-8">
-            <h1 class="flex-auto text-5xl font-bold">Pratiques</h1>
-            <div class="m-auto text-sm">
-                <label for="day">Sorti ces derniers jours ;</label>
-                <input class="border-2 border-light-gray-900 text-center timeSelection" type="number" id="day" name="day"
-                    min="0" max="100">
-            </div>
-        </div>
-        @forelse  ($practices as $practice)
-            <h2 class="mt-2 pt-8 text-sm text-gray-500">
-                Créé le {{ $practice->created_at->isoformat('D MMMM Y') }},
-                mis à jours le {{ $practice->updated_at->isoformat('D MMMM Y à HH:mm') }}
-            </h2>
-            <p class="mt-4 text-lg">{{ $practice->description }} </p>
-            <span
-                class="inline-flex items-center justify-center px-2 py-1 mr-2 text-xs font-bold leading-none text-pink-100 bg-pink-600 rounded-full">
-                {{ $practice->domain->name }}
-            </span>
-        @empty
-            <h3 class="text-blue-600 md:text-red-600">Aucune bonne pratique</h3>
-        @endforelse
-    </article>
-@endsection
+    @livewire('show-practice')
+
+@endsection --}}
