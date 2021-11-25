fc7d07ef52f6bb3fa57daec85230396a4ea8a2a4
Anthony <anthony.bouillant@cpnv.ch>
2021-11-25 20:37:32 +0100

    Create ShowPractice

---
+++ app/Http/Livewire/ShowPractice.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Carbon\Carbon;
+use Livewire\Component;
+use App\Models\Practice;
+use App\Models\PublicationState;
+
+class ShowPractice extends Component
+{
+
+    public $days = Practice::DAYS;
+
+    public function updateDays(int $days)
+    {
+        $this->days = $days;
+    }
+
+    public function render()
+    {
+        Carbon::setLocale('fr');
+        $practices = PublicationState::where('slug', 'PUB')->first()->practices()->where('updated_at', '>=', Carbon::now()->subDays(intval($this->days)))->get();
+        return view('livewire.show-practice', ['practices' => $practices])->extends('layout')->section('content');
+    }
+}
