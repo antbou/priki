bed415dfd549a13bc675b16c533b65f70d978527
Anthony Bouillant <anthony.bouillant@cpnv.ch>
2021-12-02 10:37:43 +0100

    Set default carbon format in controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,15 +2,11 @@
 
 namespace App\Http\Controllers;
 
-use Carbon\Carbon;
-use App\Models\PublicationState;
-
 
 class HomeController extends Controller
 {
     public function show()
     {
-        Carbon::setLocale('fr');
         return view('home.home');
     }
 }
