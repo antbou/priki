3a05f59479cd389507a4d7a2ed9a66d6afbe77c1
Anthony Bouillant <anthony.bouillant@cpnv.ch>
2021-12-16 09:44:23 +0100

    Show opinions

--- resources/views/practice/show.blade.php
+++ resources/views/practice/show.blade.php
@@ -17,5 +17,21 @@
             </span>
         @endif
 
+        <div class="font-bold text-xl mb-2 pt-6">Commentaires ({{ count($opinions) }})</div>
+        @foreach ($opinions as $opinion)
+            <article class="text-3xl rounded overflow-hidden border-solid border-2 my-6 px-6 py-4">
+                <div class="font-bold text-base mb-2">
+                    {{ $opinion->user()->fullname }}
+                </div>
+                <h2 class="mt-2 ptb-1 text-sm text-gray-500">
+                    Créé le {{ $opinion->created_at->isoformat('D MMMM Y') }},
+
+                </h2>
+                <p class="text-gray-700 text-base">
+                    {{ $opinion->description }}
+                </p>
+            </article>
+        @endforeach
+
     </article>
 @endsection
