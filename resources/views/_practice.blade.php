53cd2b8de3dd2a2db1658df8dc1d57f8752d2799
Anthony Bouillant <anthony.bouillant@cpnv.ch>
2022-01-11 11:15:30 +0100

    Update : hide link more details when practices state is not PUB

--- resources/views/_practice.blade.php
+++ resources/views/_practice.blade.php
@@ -4,18 +4,17 @@
         mis à jours le {{ $practice->updated_at->isoformat('D MMMM Y à HH:mm') }}
     </h2>
     <p class="mt-4 text-lg">{{ \Illuminate\Support\Str::words($practice->description, 100, $end = ' ...') }}
-        <br><a class="underline text-blue-600 hover:text-blue-800 visited:text-purple-600"
-            href="{{ route('practice', ['id' => $practice->id]) }}">Plus de détail</a>
+        @if (App\Models\Practice::isPracticeIsPublished($practice->id)->first())
+            <br><a class="underline text-blue-600 hover:text-blue-800 visited:text-purple-600"
+                href="{{ route('practice', ['id' => $practice->id]) }}">Plus de détail</a>
+        @endif
     </p>
-
     @if (!isset($hideDomain))
         <span
             class="inline-flex items-center justify-center px-2 py-1 mr-2 text-xs font-bold leading-none text-pink-100 bg-pink-600 rounded-full">
             {{ $practice->domain->name }}
         </span>
     @endif
-
-
 @empty
     <h3 class="mt-4 text-blue-600 md:text-red-600">Aucune bonne pratique</h3>
 @endforelse
