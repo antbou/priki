5bc979d5d5158d889186b6053887d438a97491bd
Anthony Bouillant <anthony.bouillant@cpnv.ch>
2022-01-13 08:38:54 +0100

    Create UserOpinion model

---
+++ app/Models/UserOpinion.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class UserOpinion extends Model
+{
+    use HasFactory;
+
+    public $timestamps = false;
+    protected $guarded = [];
+    protected $table = 'user_opinion';
+
+    public function opinion()
+    {
+        return $this->belongsTo(Opinion::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
