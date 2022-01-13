960f0962c697a8c4e909722187dd803f68d535f8
Anthony Bouillant <anthony.bouillant@cpnv.ch>
2022-01-13 10:12:46 +0100

    Update scrip
    Handle execption when dropdownButton or dropdownList don't exist

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,6 +1,8 @@
 const dropdownButton = document.querySelector("#dropdown");
 const dropdownList = document.querySelector("#dropdown + div.hidden");
 
-dropdownButton.addEventListener("click", () => {
-    dropdownList.classList.toggle("hidden");
-});
+if (dropdownButton && dropdownList) {
+    dropdownButton.addEventListener("click", () => {
+        dropdownList.classList.toggle("hidden");
+    });
+}
