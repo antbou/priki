feefbe7365c36ca3751c2f20284569daa5c88e5c
Anthony <anthony.bouillant@cpnv.ch>
2022-01-17 20:55:59 +0100

    Update referene show view
    Handle empty reference url

--- resources/views/reference/show.blade.php
+++ resources/views/reference/show.blade.php
@@ -3,9 +3,12 @@
         @foreach (App\Models\Reference::all() as $reference)
             <article class="text-3xl rounded overflow-hidden border-solid border-2 my-6 px-6 py-4">
                 {{ $reference->description }}
-                <a target="_blank"
-                    class="inline-flex items-center h-8 px-4 m-2 text-sm text-indigo-100 transition-colors duration-150 bg-indigo-700 rounded-lg focus:shadow-outline hover:bg-indigo-800"
-                    href="{{ $reference->url }}">{{ Str::limit($reference->url, $limit = 25) }}</a>
+                @empty(!$reference->url)
+                    <a target="_blank"
+                        class="inline-flex items-center h-8 px-4 m-2 text-sm text-indigo-100 transition-colors duration-150 bg-indigo-700 rounded-lg focus:shadow-outline hover:bg-indigo-800"
+                        href="{{ $reference->url }}">{{ Str::limit($reference->url, $limit = 25) }}
+                    </a>
+                @endempty
             </article>
         @endforeach
     </article>
