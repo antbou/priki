07b8fc729a4d5c15a96fda3f8c3e89eb54eb6c67
Anthony Bouillant <anthony.bouillant@cpnv.ch>
2022-01-18 12:13:57 +0100

    Update Model User
    Add role function

--- app/Models/User.php
+++ app/Models/User.php
@@ -47,4 +47,10 @@ class User extends Authenticatable
     {
         return $this->hasMany(Practice::class);
     }
+
+
+    function role()
+    {
+        return $this->belongsTo(Role::class)->first();
+    }
 }
