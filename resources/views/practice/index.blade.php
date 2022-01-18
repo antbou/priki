5640da9e5d1c56b795f3f2b5c6b6fc95666f9b39
Anthony Bouillant <anthony.bouillant@cpnv.ch>
2022-01-18 12:14:40 +0100

    Create practice index view

---
+++ resources/views/practice/index.blade.php
@@ -0,0 +1,19 @@
+<x-app-layout>
+    <article class="text-3xl max-w-prose mx-auto py-8">
+
+        @foreach ($domains as $domain)
+
+            <h3 class="bg-white p-5 border-2 rounded-lg mt-5 bold text-center">
+                {{ App\Models\Domain::find($domain->first()->domain_id)->name }} :
+            </h3>
+
+            @forelse ($domain as $practice)
+
+                @include('partials._practice', ['link' => true])
+            @empty
+                <h3 class="mt-4 text-blue-600 md:text-red-600">Aucune bonne pratique</h3>
+            @endforelse
+        @endforeach
+
+    </article>
+</x-app-layout>
