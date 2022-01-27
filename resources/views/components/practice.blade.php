f44179b932721ebbb3b406da84680fe833e40aa4
Anthony Bouillant <anthony.bouillant@cpnv.ch>
2022-01-27 14:16:07 +0100

    Update practice component
    Display title

--- resources/views/components/practice.blade.php
+++ resources/views/components/practice.blade.php
@@ -4,6 +4,7 @@
     Créé le {{ $practice->created_at->isoformat('D MMMM Y') }},
     mis à jours le {{ $practice->updated_at->isoformat('D MMMM Y à HH:mm') }}
 </h2>
+<h2>{{ $practice->title }}</h2>
 <p class="mt-4 text-lg">
     @if ($truncate)
         {{ \Illuminate\Support\Str::words($practice->description, 100, $end = ' ...') }}
